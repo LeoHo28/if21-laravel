c7c31ba7f032dd37503dbcd3a40cc8a494f661fe
leo <leonardoho_2428250030@mhs.mdp.ac.id>
2025-05-09 20:36:24 +0700

    tes

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -1,5 +1,46 @@
-<h1>Fakultas</h1>
-@foreach ($fakultas as $item)
-    {{ $item->nama }}
-    {{ $item->singkatan }}
-@endforeach
+@extends('main')
+
+@section('title', 'Fakultas')
+@section('content')
+
+ <!--begin::Row-->
+            <div class="row">
+              <div class="col-12">
+                <!-- Default box -->
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Fakultas</h3>
+                    <div class="card-tools">
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                      >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                      >
+                        <i class="bi bi-x-lg"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">@foreach ($fakultas as $item)
+                    {{ $item->nama }}
+                    {{ $item->singkatan }}
+                @endforeach
+            </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer">Footer</div>
+                  <!-- /.card-footer-->
+                </div>
+                <!-- /.card -->
+              </div>
+            </div>
+            <!--end::Row-->
+@endsection
