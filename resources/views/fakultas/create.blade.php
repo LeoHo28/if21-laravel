0ff8766af10af3721ecdfa32c093ca6bdf561258
LeoHo28 <leonardoho_2428250030@mhs.mdp.ac.id>
2025-05-27 17:09:56 +0700

    Create create.blade.php

---
+++ resources/views/fakultas/create.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+
+@section('title', 'Fakultas')
+@section('content')
+    <!--begin::Row-->
+    <div class="row">
+        <div class="col-12">
+             
+        </div>
+        <!-- /.card -->
+        </div>
+    </div>
+    <!--end::Row-->
+@endsection
